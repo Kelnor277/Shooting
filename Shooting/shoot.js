d105c45134000e2e1ce5d3ff2873b34b1243b52d
James Jones <jonesj3599@gmail.com>
2016-09-25 06:16:15 -0500

    things

--- Shooting/shoot.js
+++ Shooting/shoot.js
@@ -3,9 +3,10 @@ var weapons = {
     'Rifle': {'name':'Rifle', 'attributes':["One", "Two"], 'range':{'point': 3, 'short':25,'medium':60,'long':120, 'extreme': 200}, 'ROF': {"Single": 1, "Burst": 3, "Auto": 6}, 'damage': "1d10+3", "penetration": 0, 'damage_type': "explosive"}
 };
 
-var ranges = ['point blank', 'short', 'medium', 'long'];
+var ranges = ['point Blank', 'short', 'medium', 'long'];
 var modifiers = [30, 10, 0, -10, -30];
 var activeshots = {};
+var gmpid = "";
 
 on("ready", function() {
     apicmd.on('shoot', 'helpers for shooting', '[OPTIONS] OBJECT',
@@ -14,12 +15,23 @@ on("ready", function() {
             ['-w', '--weapon [WEAPON_NAME]', 'name of weapon'],
             ['-r', '--rof [RATE OF FIRE]', 'rate of fire'],
             ['-k', '--skill [Skill]', 'Skill of shooter'],
-            ['-m', '--modifier [Skill]', 'modifiers to the shot']],
+            ['-m', '--modifier [Skill]', 'modifiers to the shot'],
+            ['-x', '--secret [secret?]', 'keep secret']],
         shoot);
+    apicmd.on('setgm', 'helpers for shooting', '[OPTIONS] OBJECT',
+        [['-p', '--password [password]', 'shhhh secret']],
+        auth);
     apicmd.on('shootconfirm', 'Confirmed the shot', '[OPTIONS] OBJECT',
-        [['-p', '--player_id [PLAYER_ID]', "Player id"]],
+        [['-p', '--player_id [PLAYER_ID]', "Player id"],
+            ['-x', '--secret [PLAYER_ID]', "keep secret"]],
         confirmed);
 });
+
+function auth(argv, msg){
+    if(argv['opts']['password'] == "aliens"){
+        gmpid = msg.playerid;
+    }
+}
 
 function confirmed(argv, msg){
     log(argv);
@@ -37,9 +49,9 @@ function confirmed(argv, msg){
         var ROF = shot['rof'];
         var numShots = weapons[weapon_name]['ROF'][ROF];
         var damage = weapons[weapon_name]['damage'];
-        var shooter = activeshots['shootername'];
+        var shooter = shot['shootername'];
         var target = shot['target'];
-        var command = "!power {{--emote|" + shooter + " yells \"FOR THE EMPRAHHHHH!!!!!!\" --tokenid|" + shot['tokenid'] + " --format|atwill --name|" + weapon_name
+        var command = "!power {{--emote|" + shooter + " attacks their target! --tokenid|" + shot['tokenid'] + " --format|atwill --name|" + weapon_name
             + " --leftsub|" + shot['leftsub'] +
             "--Attack:|[[ [XPND][$Atk] 1d100]] vs " + target;
         command += " --?? $Atk.total >= " + target + "?? Misses:| All shots missed.";
@@ -66,6 +78,9 @@ function confirmed(argv, msg){
         }
         var attributes = weapons[weapon_name]['attributes'];
         command += "--Weapon Attributes:|" + attributes.join();
+        if(argv['opts']['secret'] == 'true'){
+            command += " --whisper|gm";
+        }
         command += "}}";
         log("Shot: " + command);
         sendChat("player|" + msg.playerid, command);
@@ -119,16 +134,21 @@ function shoot(argv, msg) {
     var range_modifier = range_calc[1];
     var id = msg.playerid;
     activeshots[id] = {'leftsub': leftsub, 'weaponName' : argv['opts']['weapon'], 'range' : range, 'range_name': range_name, 'range_modifier': range_modifier, 'rof': ROF, 'target': (skill + modifier + range_modifier), 'tokenid': argv['opts']['shooter_id'], 'shootername': shooter.get('name')};
-    var command = "[Confirm](!shootconfirm --player_id " + id + ")";
     log(command);
-    var fluff = "!power {{--emote|" + shooter.get("name") + " yells \"FOR THE EMPRAHHHHH!!!!!!\" --tokenid|" + argv['opts']['shooter_id'] + " --format|atwill --name|" + weapon['name'] + " --Weapon|" +
-        weapon['name'] + " --Range| " + range_name + ", " + range + "m " + "[" + range_modifier + "]" +
+    var fluff = "!power {{--format|atwill --name|" + weapon['name'] + " --Weapon:|" +
+        weapon['name'] + " --Range:| " + range_name + " (+" + range_modifier + ")" + ", " + range + "m " +
         " --leftsub|" + leftsub +
-        " --ROF|" + ROF + " Ammo Used: " + weapon['ROF'][ROF] +
+        " --ROF:|" + ROF + " Ammo Used: " + weapon['ROF'][ROF] +
         " --Roll Target:|Skill: " + skill + ", Difficulty Modifier: " + modifier + ", Range Modifier:" + range_modifier + ", Total " + (modifier + skill + range_modifier) +
-        " --Target:| " + target.get('name') +
-        ".}}";
+        " --Target:| " + target.get('name') + ".";
+    var command = "";
+    if(argv['opts']['secret'] == 'true'){
+        fluff += " --whisper|gm";
+        command += "/w gm ";
+    }
+    fluff += "}}";
     //{{--name|" + weapon['name'] + " " + range_name + "Modifier: " + range_modifier + "}}
+    command += "[Confirm](!shootconfirm --player_id " + id + " --secret true)";
     var message = fluff + "\n" + command;
     sendChat("player|" + msg.playerid, message);
     // sendChat("player|" + msg.playerid, command);
@@ -148,9 +168,9 @@ function distance(shooter, target){
     var gridSize = 70;
 
     var lDist = Math.abs(shooter.get("left")-target.get("left"))/gridSize;
-    lDist = lDist * 5;
+    lDist = lDist * 2;
     var tDist = Math.abs(shooter.get("top")-target.get("top"))/gridSize;
-    tDist = tDist * 5;
+    tDist = tDist * 2;
     var dist = 0;
 
     dist = Math.sqrt(lDist*lDist + tDist*tDist);
